460682877b1c2922b4e591290b48a6d9568c1dc7
daitennn <baseball.11.daisuke@gmail.com>
2023-01-07 13:32:09 +0900

    answer delete

--- stdin.py
+++ stdin.py
@@ -1,8 +1,6 @@
 import random
 import sys
 
-x = random.randint(2, 10)
-print(x)
 
 sys.stdout.buffer.write(b"What do you choice(min)?\n")
 sys.stdout.flush()
@@ -14,12 +12,21 @@ sys.stdout.flush()
 m = sys.stdin.buffer.readline()
 print("m :" + str(m.decode()))
 
+
+def decideNOrM(n, m):
+    return n > m
+
+
 for i in range(0, 5):
     print("Decide on the minimum and maximum numbers of your choice.(up to 5 times)\n")
     sys.stdout.buffer.write(b"What do you choice?\n")
     sys.stdout.flush()
     res = sys.stdin.buffer.readline()
     print("decide: " + str(res.decode()))
+
+    if (decideNOrM(n, m)):
+        print("最小値と最大値をもう一度やり直してください。")
+        break
 
     computer = random.randint(int(n), int(m))
 
